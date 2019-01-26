dffbccf669024c3924f3e936db574cc5bce53d7e
Morris Lin <snoopy90338@gmail.com>
2019-01-26 19:01:40 +0800

    add scss hot-loader and build

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,22 @@
 const webpack = require('webpack')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
-	entry: ['./src/js/index.js'],
+	entry: ['./src/js/index.js', './src/scss/style.scss'],
 	module: {
 		rules: [
 			{
 				test: /\.(js|jsx)$/,
 				exclude: /node_modules/,
 				use: ['babel-loader']
+			},
+			{
+				test: /\.scss$/,
+				use: [
+					process.env.NODE_ENV !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader,
+					'css-loader',
+					'sass-loader'
+				]
 			}
 		]
 	},
@@ -20,7 +29,8 @@ module.exports = {
 		filename: 'bundle.js'
 	},
 	plugins: [
-		new webpack.HotModuleReplacementPlugin()
+		new webpack.HotModuleReplacementPlugin(),
+		new MiniCssExtractPlugin({ filename: 'style.css' })
 	],
 	devServer: {
 		contentBase: './public',
