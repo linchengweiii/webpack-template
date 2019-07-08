0bb1d54147a588dd07903b2b4e0db83d199606a5
Morris Lin <snoopy90338@gmail.com>
2019-07-08 22:22:24 +0800

    hot-fix

--- webpack.config.js
+++ webpack.config.js
@@ -21,8 +21,7 @@ module.exports = {
 					{
 						loader: MiniCssExtractPlugin.loader,
 						options: {
-							hmr: process.env.NODE_ENV === 'development',
-							reloadAll: true
+							hmr: process.env.NODE_ENV === 'development'
 						}
 					},
 					'css-loader',
